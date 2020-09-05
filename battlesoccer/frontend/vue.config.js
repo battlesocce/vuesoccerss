5e17f1c8cc960272f9a6a4f77177373863735570
battlesocce <70088282+battlesocce@users.noreply.github.com>
2020-09-05 21:05:57 +0530

    Update vue.config.js

--- battlesoccer/frontend/vue.config.js
+++ battlesoccer/frontend/vue.config.js
@@ -2,9 +2,8 @@ const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
     // on Windows you might want to set publicPath: "http://127.0.0.1:8080/" 
-    publicPath: "static/",
+   publicPath: "http://127.0.0.1:8080/",
     outputDir: './dist/',
-    assetsDir: 'static',
 
     chainWebpack: config => {
 
@@ -20,6 +19,10 @@ module.exports = {
 
         config.resolve.alias
             .set('__STATIC__', 'static')
+        
+             module.exports = {
+           assetsDir: 'static'
+         }
 
         config.devServer
             // the first 3 lines of the following code have been added to the configuration
