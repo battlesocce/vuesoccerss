a81829183267d555ed7599c79e00ee8d70d2f3be
battlesocce <70088282+battlesocce@users.noreply.github.com>
2020-09-06 17:01:36 +0530

    Update settings.py

--- battlesoccer/battlesoccer/settings.py
+++ battlesoccer/battlesoccer/settings.py
@@ -160,9 +160,14 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # django.contrib.sites
 SITE_ID = 1
 # django-allauth
-ACCOUNT_EMAIL_VERIFICATION = "none"
 ACCOUNT_EMAIL_REQUIRED = (True)
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'battlesoccers@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sujitha312'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 
